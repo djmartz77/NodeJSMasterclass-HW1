f13e4def8c0844f66362796ae1bf853f9f9e9fe5
Dan Martz <djmartz77@gmail.com>
2018-11-06 10:56:26 -0600

    POST requests to /hello result in personalized or default greeting based on query string parameters. other verbs result in 'Not a POST'. /ping returns 200. unknown requests return 404.

--- handlers.js
+++ handlers.js
@@ -6,6 +6,19 @@
 
 let handlers = {};
 
+handlers.hello = function(data,callback){
+	if(data.method === 'POST'){
+		if(data.queryStringObject.name){
+			callback(200, {greeting: 'Hello, ' +data.queryStringObject.name})
+		} else {
+			callback(200, {greeting: 'Hello, friend!'});
+		}
+		
+	} else {
+		callback(200, {result: 'Not a POST'});
+	}
+};
+
 handlers.ping = function(data,callback){
 	callback(200);
 };
--- index.js
+++ index.js
@@ -42,8 +42,64 @@ httpsServer.listen(config.httpsPort, function(){
 
 // Unified server logic
 const unifiedServer = function(req,res) {
-	console.log('reached unifiedServer');
-	res.end('Hello dude.');
+	// parse the URL and get the path
+	const parsedUrl = url.parse(req.url, true);
+	const path = parsedUrl.pathname.replace(/^\/+|\/+$/g,'');
+
+	// get the HTTP method
+	const httpMethod = req.method.toUpperCase();
+
+	// get and parse the query string, if any
+	const queryStringObj = parsedUrl.query;
+
+	const headers = req.headers;
+
+	// get the message payload, if any
+	const decoder = new StringDecoder('utf-8');
+	let buffer = '';
+
+	req.on('data',function(data){
+		buffer += decoder.write(data);
+	});
+
+	req.on('end',function(){
+		buffer += decoder.end()
+		// if HTTP method is POST, return a greeting
+		const chosenHandler = typeof(router[path]) !== 'undefined' ? router[path] : handlers.notFound;
+
+		// Construct data object to send to the handler
+		const data = {
+			'path' : path,
+			'queryStringObject' : queryStringObj,
+			'method' : httpMethod,
+			'headers' : headers,
+			'payload' : buffer,
+		};
+
+		// Route the request to the handler specified in the router
+		chosenHandler(data, function(statusCode,payload){
+			// Use the status code called back by the handler, or default to 200
+			statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+
+			// Use the payload called back by the handler, or default to an empty object
+			payload = typeof(payload) === 'object' ? payload : {};
+			payload = payload === null ? {} : payload;
+
+			// Convert the payload to a string
+			let payloadString = JSON.stringify(payload);
+
+			// Send the response
+			res.setHeader('Content-Type','application/json');
+			res.writeHead(statusCode);
+			res.end(payloadString);
+
+
+			// Log the request
+			console.log('Returning this response:', statusCode, payloadString);
+		});
+
+	});
+
 }
 
 
